16e75fcc9fbb7b5ac1c97d28bf13cca0f2b033bd
Matthew Rueben <mrueben@nmsu.edu>
2024-07-29 09:07:18 -0700

    Add field to tester object for setting max number of pairs to test.

--- src/main/java/matthew_rueben/pairing/performance/SockPairerTester.java
+++ src/main/java/matthew_rueben/pairing/performance/SockPairerTester.java
@@ -16,8 +16,15 @@ public class SockPairerTester
 {
     private static Logger logger = LogManager.getLogger();
 
-    private static void testAlgorithm(PairingAlgorithm pairer) {
-        int numPairs = 30;
+    private int maxNumOfPairs = 10;
+
+    public void setMaxNumOfPairsTo(int newMax)
+    {
+        this.maxNumOfPairs = newMax;
+    }
+
+    public void testAlgorithm(PairingAlgorithm pairer) {
+        int numPairs = this.maxNumOfPairs; // Later will add sweep functionality, from 1 to max.
         List<Matchable> socks = new ArrayList<>(numPairs*2);
         for (int pair = 0; pair < numPairs; pair++) {
             socks.add(new MatchableByNumber(pair)); // First sock in pair.
@@ -32,10 +39,13 @@ public class SockPairerTester
 
     public static void main(String[] args)
     {
+        SockPairerTester tester = new SockPairerTester();
+        tester.setMaxNumOfPairsTo(30);
+
         //testAYGMatcher.sweepByNumPairs(minNumPairs, maxNumPairs);
 
-        testAlgorithm(new AsYouGoPairer());
-        testAlgorithm(BatchPairer.makeBatchPairerWithBatchSize(5));
-        testAlgorithm(new AtTheEndPairer());
+        tester.testAlgorithm(new AsYouGoPairer());
+        tester.testAlgorithm(BatchPairer.makeBatchPairerWithBatchSize(5));
+        tester.testAlgorithm(new AtTheEndPairer());
     }
 }
