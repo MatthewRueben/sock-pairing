62f74f4e229656e98322edf600fd177749e8ce6f
Matthew Rueben <mrueben@nmsu.edu>
2024-07-29 08:54:51 -0700

    Add logging to algorithm tester and clean up old comments.

--- src/main/java/matthew_rueben/pairing/performance/SockPairerTester.java
+++ src/main/java/matthew_rueben/pairing/performance/SockPairerTester.java
@@ -1,6 +1,8 @@
 package matthew_rueben.pairing.performance;
 
 import matthew_rueben.pairing.algorithms.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,12 +14,8 @@ import java.util.ArrayList;
  */
 public class SockPairerTester
 {
-    //private MatchesSocks sockMatcher;
+    private static Logger logger = LogManager.getLogger();
 
-    //public SockPairerTester(MatchesSocks sockMatcher)
-    //{
-    //    this.sockMatcher = sockMatcher;  
-    //}
     private static void testAlgorithm(PairingAlgorithm pairer) {
         int numPairs = 30;
         List<Matchable> socks = new ArrayList<>(numPairs*2);
@@ -25,33 +23,19 @@ public class SockPairerTester
             socks.add(new MatchableByNumber(pair)); // First sock in pair.
             socks.add(new MatchableByNumber(pair)); // Second sock in pair.
         }
-        //System.out.println(socks);
-        //System.out.println();
         Collections.shuffle(socks); // Randomize.
-        //System.out.println(socks);
-        
-        pairer.pair(socks);
+        logger.debug(socks);
+
+        ComparisonsCounts counts = pairer.pair(socks);
+        logger.info("Total comparisons: {}", counts.total);
     }
 
     public static void main(String[] args)
     {
-        /* 
-         * Each SockPairerTester object puts one sockMatcher
-         * through its paces. Make several Tester 
-         * objects, one for each sockMatcher. Each 
-         * one will try the sockMatcher on (1) all 
-         * permutations of (2) each number of pairs 
-         * of socks from 1 to 100. 
-         */
-        
         //testAYGMatcher.sweepByNumPairs(minNumPairs, maxNumPairs);
-        //testAYGMatcher.test(numPairs);
 
         testAlgorithm(new AsYouGoPairer());
         testAlgorithm(BatchPairer.makeBatchPairerWithBatchSize(5));
         testAlgorithm(new AtTheEndPairer());
-
-        
-        
     }
 }
