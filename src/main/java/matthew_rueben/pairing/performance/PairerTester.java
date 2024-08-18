eb72ec0b8cf9274554b7022993c28cf3e2a8f0a9
Matthew Rueben <mrueben@nmsu.edu>
2024-08-17 20:45:39 -0700

    Get generics working and switch to a pool of objects to be paired.

---
+++ src/main/java/matthew_rueben/pairing/performance/PairerTester.java
@@ -0,0 +1,84 @@
+package matthew_rueben.pairing.performance;
+
+import matthew_rueben.pairing.algorithms.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import static com.google.common.collect.Collections2.orderedPermutations;
+
+/**
+ * Tests pairing algorithms.
+ *
+ * @author Matt Rueben
+ */
+public class PairerTester<CM extends Comparable<CM> & Matchable<CM>> // CM is for Comparable and Matchable.
+{
+    private static final Logger logger = LogManager.getLogger();
+
+    private final MatchablePool<CM> pool;
+    
+    PairerTester(MatchablePool<CM> pool) {this.pool = pool;}
+
+    private void getPermutations(List<CM> in)
+    {
+
+    }
+
+    private List<CM> makePairs(int numPairs)
+    {
+        List<CM> pairsList = new ArrayList<>(numPairs*2);
+        
+        for (int i = 1; i <= numPairs; i++)
+        {
+            CM instance1of2 = pool.getNewUnmatchedInstance();
+            pairsList.add(instance1of2);
+            CM instance2of2 = pool.getNewInstanceThatMatches(instance1of2);
+            pairsList.add(instance2of2);
+        }
+        return pairsList;
+    }
+
+    // Might want to pass in *all* the pairers at once so we don't have to do permutations multiple times.
+    public void testAlgorithm(final PairingAlgorithm pairer, int maxNumOfPairs)
+    {
+        logger.info("Testing {}.", pairer);
+
+        int numPairs = maxNumOfPairs; // Later will add sweep functionality, from 1 to max.
+        List<CM> pairs = makePairs(numPairs);
+
+        for (List<CM> reorderedPairs : orderedPermutations(pairs))
+        {
+            List<CM> copyOfReorderedPairs = new ArrayList<>(numPairs*2);
+            for (CM halfOfAPair : reorderedPairs)
+            {
+                CM copyOfHalfAPair = pool.getNewInstanceThatMatches(halfOfAPair);
+                copyOfReorderedPairs.add(copyOfHalfAPair);
+            }
+            logger.debug(copyOfReorderedPairs);
+
+            ComparisonsCounts comparisons = pairer.pair(copyOfReorderedPairs);
+            logger.info("Comparisons: {}.", comparisons.total);
+
+            pool.removeTheseInstances(copyOfReorderedPairs);
+        }
+        pool.clearAllInstances();
+    }
+    
+    public static void main(String[] args)
+    {
+        MatchableByNumberPool pool = new MatchableByNumberPool();
+        PairerTester<MatchableByNumber> tester = new PairerTester<>(pool);
+
+        //testAYGMatcher.sweepByNumPairs(minNumPairs, maxNumPairs);
+
+        int maxNumOfPairs = 3;
+
+        tester.testAlgorithm(new AsYouGoPairer(), maxNumOfPairs);
+        //tester.testAlgorithm(BatchPairer.makeBatchPairerWithBatchSize(5));
+        tester.testAlgorithm(new AtTheEndPairer(), maxNumOfPairs);
+    }
+}
